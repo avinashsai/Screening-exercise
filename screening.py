7b5f974be98ad7338e69c4c1a6df0644eb0c6a0f
Avinash Madasu <avinash.sai001@gmail.com>
2022-04-04 19:13:29 -0400

    Indendation

--- screening.py
+++ screening.py
@@ -3,12 +3,8 @@ from trainer import NewTrainer
 
 
 # Check the data types of inputs to the train function
-@pytest.mark.parametrize('x', [[[1, 2, 3, 4]],
-							[[0.11, 0.2, 0.3, 0.4]],
-							[[0.11, 1, 0.3, 0.4]]])
-@pytest.mark.parametrize('y', [[[1, 2, 3, 4]],
-							[[1, 2, 3, 4]]],
-							[[0.11, 0.2, 0.3, 0.4]])
+@pytest.mark.parametrize('x', [[[1, 2, 3, 4]], [[0.11, 0.2, 0.3, 0.4]], [[0.11, 1, 0.3, 0.4]]])
+@pytest.mark.parametrize('y', [[[1, 2, 3, 4]], [[1, 2, 3, 4]]], [[0.11, 0.2, 0.3, 0.4]])
 def test_types_train(x, y):
     trainer_class = NewTrainer()
     with pytest.raises(Exception) as wrong_types:
@@ -16,9 +12,7 @@ def test_types_train(x, y):
 
 
 # Check the data types of inputs to the test function
-@pytest.mark.parametrize('x', [[[1, 2, 3, 4]],
-							[[0.11, 0.2, 0.3, 0.4]],
-							[[0.11, 1, 0.3, 0.4]]])
+@pytest.mark.parametrize('x', [[[1, 2, 3, 4]], [[0.11, 0.2, 0.3, 0.4]], [[0.11, 1, 0.3, 0.4]]])
 def test_types_test(x, y):
     test_class = NewTrainer()
     with pytest.raises(Exception) as wrong_types:
@@ -26,12 +20,8 @@ def test_types_test(x, y):
 
 
 # Check if the input sizes match in the train function
-@pytest.mark.parametrize('x', [[[1, 2, 3]],
-							[[0.11, 0.2, 0.3, 0.4]],
-							[[]]])
-@pytest.mark.parametrize('y', [[[1, 2, 3, 4]],
-							[[1, 2, 3]]],
-							[[0.11, 0.2, 0.3, 0.4]])
+@pytest.mark.parametrize('x', [[[1, 2, 3]], [[0.11, 0.2, 0.3, 0.4]], [[]]])
+@pytest.mark.parametrize('y', [[[1, 2, 3, 4]], [[1, 2, 3]]], [[0.11, 0.2, 0.3, 0.4]])
 def check_shapes(x, y):
     trainer_class = NewTrainer()
     with pytest.raises(Exception) as wrong_shapes:
@@ -39,9 +29,7 @@ def check_shapes(x, y):
 
 
 # Check the number of arguments in the train function
-@pytest.mark.parametrize('x', [[[1, 2, 3, 4]],
-							[[0.11, 0.2, 0.3, 0.4]],
-							[[0.11, 1, 0.3, 0.4]]])
+@pytest.mark.parametrize('x', [[[1, 2, 3, 4]], [[0.11, 0.2, 0.3, 0.4]], [[0.11, 1, 0.3, 0.4]]])
 def test_arguments_train(x):
     trainer_class = NewTrainer()
     with pytest.raises(Exception) as wrong_arguments_types:
@@ -49,12 +37,8 @@ def test_arguments_train(x):
 
 
 # Check the number of arguments of inputs to the test function
-@pytest.mark.parametrize('x', [[[1, 2, 3, 4]],
-							[[0.11, 0.2, 0.3, 0.4]],
-							[[0.11, 1, 0.3, 0.4]]])
-@pytest.mark.parametrize('y', [[[1, 2, 3, 4]],
-							[[1, 2, 3, 4]]],
-							[[0.11, 0.2, 0.3, 0.4]])
+@pytest.mark.parametrize('x', [[[1, 2, 3, 4]], [[0.11, 0.2, 0.3, 0.4]], [[0.11, 1, 0.3, 0.4]]])
+@pytest.mark.parametrize('y', [[[1, 2, 3, 4]], [[1, 2, 3, 4]]], [[0.11, 0.2, 0.3, 0.4]])
 def test_arguments_train(x, y):
     trainer_class = NewTrainer()
     with pytest.raises(Exception) as wrong_arguments_types:
